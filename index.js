088b9bb64a11d02208c53d637e006ab001e27a9c
Álvaro García <alvaro@reboot.academy>
2022-02-01 23:11:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
+
+;(async function dbConnect() {
+  try {
+    await mongoose.connect('mongodb://localhost:27017', {
+      dbName: 'memenstagram'
+    })
+    console.log('Conectado a la DB')
+  } catch (error) {
+    throw new Error ()
+  }
+})()
+
+const app = express()
+
+try  {
+  app
+    .use(cors())
+    .use(morgan('dev'))
+    .use(express.json())
+    // .use('/api', require('./src/routes'))
+    .use('/', (req, res) => {
+      res.send('Welcome to Memestagram API')
+    })
+    .listen(3000, () => {
+      console.log('Server golisneando')
+    })
+} catch (error) {
+  throw new Error (`Can't start Express`)
+}
+
